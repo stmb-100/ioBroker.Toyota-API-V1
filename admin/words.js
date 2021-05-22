6063986f182e7fe6c2d15534246ed5f72f756d8d
Stefan Mühlbauer <github@stmb-mail.de>
2021-05-22 13:59:53 +0200

    2. commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"toyota-api-v1 adapter settings": {
+		"en": "Adapter settings for toyota-api-v1",
+		"de": "Adaptereinstellungen für toyota-api-v1",
+		"ru": "Настройки адаптера для toyota-api-v1",
+		"pt": "Configurações do adaptador para toyota-api-v1",
+		"nl": "Adapterinstellingen voor toyota-api-v1",
+		"fr": "Paramètres d'adaptateur pour toyota-api-v1",
+		"it": "Impostazioni dell'adattatore per toyota-api-v1",
+		"es": "Ajustes del adaptador para toyota-api-v1",
+		"pl": "Ustawienia adaptera dla toyota-api-v1",
+		"zh-cn": "toyota-api-v1的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
