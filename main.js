791a536674d4e764c6f984c108b960c764aca52b
Stefan Mühlbauer <54959405+stmb-100@users.noreply.github.com>
2021-08-23 08:41:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,8 @@ class ToyotaApiV1 extends utils.Adapter {
 		// this.config:
 		this.log.info("config option1: " + this.config.option1);
 		this.log.info("config option2: " + this.config.option2);
-
+		this.log.info("config option3: " + this.config.option3);
+		//this.log.info("config option2: " + this.config.option2);
 		/*
 		For every state in the system there has to be also an object of type state
 		Here a simple template for a boolean variable named "testVariable"
